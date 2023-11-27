567e277e23cb476906747e34f581cd46409de774
Lola Cohidon <116801009+LoLuX22@users.noreply.github.com>
2023-11-27 16:41:00 +0100

    PokemonManager.php/class PokemonManager
    Création de la classe Pokemon Manager et ajout des héritages et dépendances

--- models/PokemonManager.php
+++ models/PokemonManager.php
@@ -1,6 +1,11 @@
 <?php 
     require_once 'models/Model.php'; 
     require_once 'models/Pokemon.php'; 
-  
+
+    // Classe PokemonManager
+    class PokemonManager extends Model
+    {
+    }
+        
           
 ?>
