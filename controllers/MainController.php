9f4f746c6b49cc1a2149fcdf754f8ea0844343c7
Lola Cohidon <116801009+LoLuX22@users.noreply.github.com>
2023-11-27 14:26:40 +0100

    Ajout des fichiers

---
+++ controllers/MainController.php
@@ -0,0 +1,9 @@
+<?php 
+require_once "views\View.php";
+
+public function Index() : void {
+    $indexView = new View('Index');
+    $indexView->generer(['nomDresseur' => "Red"]);
+}
+?>
+
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+    require_once 'controller/MainController.php'
+
+    $controller = new MainController();
+    $controller->index();
+?>
