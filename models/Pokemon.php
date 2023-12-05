99cce3374f9f476e38a464cb24057efb7ab52ae9
Lola Cohidon <lola.cohidon@iut-dijon.u-bourgogne.fr>
2023-12-05 09:05:03 +0100

    Pokemon.php/function hydrate
    Implémentation de l'hydratation dans la classe Pokemon

--- models/Pokemon.php
+++ models/Pokemon.php
@@ -80,5 +80,15 @@ class Pokemon
     public function setUrlImg(string $urlImg) {
         $this->urlImg = $urlImg;
     }
+
+    // Fonction permettant d'hydrater un objet Pokemon
+    public function hydrate(array $donnees) {
+        foreach ($donnees as $key => $value) {
+            $method = 'set'.ucfirst($key);
+            if (method_exists($this,$method)) {
+                $this->$method($value);
+            }
+        }
+    }
 } 
 ?>
