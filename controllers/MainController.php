c0fa3a607619a112c3caf5780998e30d67b0bf0e
Lola Cohidon <lola.cohidon@iut-dijon.u-bourgogne.fr>
2023-12-05 09:51:13 +0100

    MainController.php/function Index

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -1,11 +1,22 @@
 <?php 
 require_once "views\View.php";
+require_once "models/PokemonManager.php";
 
+    // Classe MainController
     class MainController{
-    public function Index() : void {
-        $indexView = new View('Index');
-        $indexView->generer(['nomDresseur' => "Red"]);
-    }
+        // Fonction Index
+        public function Index() : void 
+        {
+            $indexView = new View('Index');
+
+            // Création d'un objet PokemonManager
+            $pokemonManager = new PokemonManager();
+            // Récupération de tous les pokemons
+            $fist = $pokemonManager->getById(1);
+            $other = $pokemonManager->getById(8);
+            // Génération de la vue Index
+            $indexView->generer(['nomDresseur' => "Red"]);
+        }
 }
 ?>
 
